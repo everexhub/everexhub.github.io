c38ca0ebce49799c2c533792c995ba82385f15ca
everexhub <162891512+everexhub@users.noreply.github.com>
2024-03-10 18:55:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#countdown').countdown('2024/06/01', function (event) {
+        $(this).html(event.strftime(
+            '<div><span class="number">%w</span> <span class="unit">Weeks</span></div> ' +
+            '<div><span class="number">%d</span> <span class="unit">Days</span></div> ' +
+            '<div><span class="number">%H</span> <span class="unit">Hours</span></div> ' +
+            '<div><span class="number">%M</span> <span class="unit">Minutes</span></div> ' +
+            '<div><span class="number">%S</span> <span class="unit">Seconds</span></div>'
+        ));
+
+        // Adding animation only to the changing numbers
+        $('.number').addClass('fadeInNumber');
+    });
+});
